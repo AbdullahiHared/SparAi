80409d356d1385614345958db8aacc4a4fccdcc7
Kiwi <muhammadshamaeem@example.com>
2024-02-03 22:10:04 +0100

    add logins

--- src/LoginForms.jsx
+++ src/LoginForms.jsx
@@ -4,12 +4,30 @@ export default function LoginForms() {
     const greetingTitle = "AI-Enhanced Savings Start Now!";
     const greetingTitleDescription = "Utilize our SparAi to automate a personalized schedule, discover budget-friendly food, and receive smart tips to save on energy";
 
-
     return (
         <>
-            <div className={"loginHeading"}>
+            <div className={"login-heading"}>
                 <h1>{greetingTitle}</h1>
                 <p>{greetingTitleDescription}</p>
+            </div>
+            <div className={"get-started"}>
+                Get Started
+            </div>
+            <div className={"login"}>
+                <div className={"google"}>
+                    <img src={"src/assets/google_logo.png"} alt={"google logo"}/>
+                    <p>Login with Google</p>
+                </div>
+                <div className={"facebook"}>
+                    <img src={"src/assets/facebook_logo.png"} alt={"facebook logo"}/>
+                    <p>Login with Facebook</p>
+                </div>
+                <a href={"https://github.com/Miishri"} target="_blank">
+                    <div className={"github"}>
+                        <img src={"src/assets/github_logo.png"} alt={"github logo"}/>
+                        <p>Login with Github</p>
+                    </div>
+                </a>
             </div>
         </>
     )
