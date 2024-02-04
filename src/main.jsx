ab1c772759b79f7b3cef1ab1c7d25cf01f5995a4
Kiwi <muhammadshamaeem@example.com>
2024-02-04 21:24:54 +0100

    change to dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,11 @@ import './style.css'
 
 import {BrowserRouter} from "react-router-dom";
 import Routers from "./extras/Routers.jsx";
+import Dashboard from "./home/Dashboard.jsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Routers />
-      </BrowserRouter>
+      <Dashboard />
   </React.StrictMode>,
 )
